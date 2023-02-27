af787009fc9ec515ee6b3c9c65a70a3cf26cc596
JwstFeed <122465126+JwstFeed@users.noreply.github.com>
2023-02-27 19:53:30 +0200

    Delete FeedPageSettings.cs

--- JwstFeed/Models/FeedPageSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace JwstFeed.Models;
-
-public class FeedPageSettings : BasePageSettings
-{
-    public ResultFilter Filter { get; set; }
-
-    public string SearchTerm { get; set; } = string.Empty;
-}
