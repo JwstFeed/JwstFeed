c265c5cf4fdc3505d9c2de11ff7e0f0603b0d44e
JwstFeed <122465126+JwstFeed@users.noreply.github.com>
2023-02-27 21:09:09 +0200

    Update ScheduleController.cs

--- JwstFeed/Controllers/ScheduleController.cs
+++ JwstFeed/Controllers/ScheduleController.cs
@@ -1,6 +1,5 @@
-﻿using JwstFeed.Models;
-using JwstFeed.Models.Extensions;
-using JwstFeed.Models.Providers;
+using JwstFeed.Models.Entities;
+using JwstFeed.Models.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace JwstFeed.Controllers;
@@ -8,7 +7,7 @@ namespace JwstFeed.Controllers;
 public class ScheduleController : Controller
 {
     #region Data Members
-    private int pageSize { get; } = 250;
+    private int pageSize { get; } = 200;
     #endregion
 
     #region Public Methods
@@ -30,89 +29,18 @@ public class ScheduleController : Controller
     #region Private Methods
     private ObservationSchedule getObservationSchedule(int pageNumber)
     {
-        PaginatedList<Observation> observations = getObservations(pageNumber);
-        TargetObservation currentObservation = getCurrentObservation(observations);
-        TargetObservation nextObservation = getNextObservation(observations);
+        ObservationScheduleService service = getService(pageNumber);
 
         return new ObservationSchedule()
         {
-            ObservingNow = currentObservation,
-            ObservingNext = nextObservation,
-            Observations = observations
+            ObservingNow = service.GetCurrentObservation(),
+            ObservingNext = service.GetNextObservation(),
+            Observations = service.GetAllObservations()
         };
     }
 
-    private TargetObservation getCurrentObservation(IReadOnlyList<Observation> observations)
-    {
-        var currentObservation = observations
-            .Select((o, i) => new
-            {
-                StartTime = o.ScheduledStartTime,
-                EndTime = getEndTime(o),
-                Observation = o,
-                RowNumber = i
-            })
-            .FirstOrDefault(o => DateTime.UtcNow.Between(o.StartTime, o.EndTime));
-
-        return new TargetObservation()
-        {
-            TargetName = currentObservation?.Observation.TargetName ?? "None",
-            VisitID = currentObservation?.Observation.VisitID ?? string.Empty,
-            RowNumber = currentObservation?.RowNumber ?? 0,
-        };
-    }
-
-    private TargetObservation getNextObservation(IReadOnlyList<Observation> observations)
-    {
-        var nextObservation = observations
-            .Select((o, i) => new
-            {
-                StartTime = o.ScheduledStartTime,
-                EndTime = getEndTime(o),
-                Observation = o,
-                RowNumber = i
-            })
-            .LastOrDefault(o =>
-            {
-                return o.StartTime > DateTime.UtcNow
-                       &&
-                       !DateTime.UtcNow.Between(o.StartTime, o.EndTime);
-            });
-
-        return new TargetObservation()
-        {
-            TargetName = nextObservation?.Observation.TargetName ?? "None",
-            VisitID = nextObservation?.Observation.VisitID ?? string.Empty,
-            RowNumber = nextObservation?.RowNumber ?? 0,
-        };
-    }
-
-    private DateTime getEndTime(Observation observation)
-    {
-        int days = observation.Duration.Split('/')[0].ToInt();
-        string[] timespan = observation.Duration.Split('/')[1].Split(':');
-        int hours = timespan[0].ToInt();
-        int minutes = timespan[1].ToInt();
-        int seconds = timespan[2].ToInt();
-
-        return observation
-            .ScheduledStartTime
-            .AddDays(days)
-            .AddHours(hours)
-            .AddMinutes(minutes)
-            .AddSeconds(seconds);
-    }
-
-    private PaginatedList<Observation> getObservations(int pageNumber)
-    {
-        BasePageSettings pageSettings = new ObservationPageSettings()
-        {
-            PageSize = this.pageSize,
-            PageNumber = pageNumber
-        };
-
-        return new FeedProvider()
-            .GetObservations(pageSettings);
-    }
+    private ObservationScheduleService getService(int pageNumber)
+        =>
+        new ObservationScheduleService(pageNumber, this.pageSize);
     #endregion
 }
