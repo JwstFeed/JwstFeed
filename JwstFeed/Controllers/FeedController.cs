e663cd0b3d659f1f7e33d0321231a957167a000a
JwstFeed <122465126+JwstFeed@users.noreply.github.com>
2023-03-15 19:46:47 +0200

    Update FeedController.cs

--- JwstFeed/Controllers/FeedController.cs
+++ JwstFeed/Controllers/FeedController.cs
@@ -1,137 +1,102 @@
-﻿using JwstFeed.Models;
-using JwstFeed.Models.FeedItems;
-using JwstFeed.Models.Providers;
+using JwstFeed.Models.Entities;
+using JwstFeed.Models.Services;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
 
 namespace JwstFeed.Controllers;
 
 public class FeedController : Controller
 {
-    #region Data Members
-    private string filterCookieName { get; } = "lastFilter";
-    private int pageSize { get; } = 75;
-    #endregion
+	#region Data Members
+	private string filterCookieName { get; } = "lastFilter";
+	private int pageSize { get; } = 75;
+	#endregion
 
-    #region Public Methods
-    public IActionResult Index()
-    {
-        string lastFilter = getFilterFromCookie();
+	#region Public Methods
+	public IActionResult Index()
+	{
+        UserPreferences userNewPreferences = getDefaultUserPreferences();
 
-        UserPreferences userNewPreferences = generateUserPreferences(
-            currentFilter: lastFilter,
-            searchTerm: string.Empty,
-            pageNumber: 1);
+        return View("ShowFeed", userNewPreferences);
+	}
+
+	public IActionResult ShowFeed(string currentFilter, int pageNumber = 1, string searchTerm = "")
+	{
+		UserPreferences userNewPreferences = generateUserPreferences(
+			pageNumber: pageNumber > 0 ? pageNumber : 1,
+			currentFilter: currentFilter,
+			searchTerm: searchTerm);
+
+		return View(userNewPreferences);
+	}
+
+    public IActionResult ShowFilteredFeed()
+	{
+        UserPreferences userNewPreferences = getDefaultUserPreferences();
 
         return View("ShowFeed", userNewPreferences);
     }
 
-    public IActionResult ShowFeed(string currentFilter, int pageNumber = 1, string searchTerm = "")
-    {
-        UserPreferences userNewPreferences = generateUserPreferences(
-            currentFilter: currentFilter,
-            pageNumber: pageNumber,
-            searchTerm: searchTerm);
+    public IActionResult Search()
+	{
+        UserPreferences userNewPreferences = getDefaultUserPreferences();
 
-        return View(userNewPreferences);
+        return View("ShowFeed", userNewPreferences);
     }
 
     [HttpPost]
-    public IActionResult ShowFilteredFeed(UserPreferences userPreferences)
-    {
-        saveFilterInCookie(userPreferences.Filter);
-        PaginatedList<IFeedItem> currentPageFeedItems = getFilteredList(userPreferences.Filter, pageNumber: 1);
-        UserPreferences userNewPreferences = getUserPreferences(currentPageFeedItems, userPreferences.Filter);
+	public IActionResult ShowFilteredFeed(UserPreferences userPreferences, string btnValue = "")
+	{
+		handleFilterMemory(userPreferences, btnValue);
+		UserPreferences userNewPreferences = convertFilterToUserPrefs(userPreferences.Filter);
 
-        return View("ShowFeed", userNewPreferences);
-    }
+		return View("ShowFeed", userNewPreferences);
+	}
 
-    [HttpPost]
-    public IActionResult Search(string currentFilter, string searchTerm)
-    {
-        UserPreferences userNewPreferences = generateUserPreferences(
-            currentFilter: currentFilter,
-            pageNumber: 1,
-            searchTerm: searchTerm);
+	[HttpPost]
+	public IActionResult Search(string currentFilter, string searchTerm)
+	{
+		UserPreferences userNewPreferences = generateUserPreferences(
+			currentFilter: currentFilter,
+			pageNumber: 1,
+			searchTerm: searchTerm);
 
-        return View("ShowFeed", userNewPreferences);
-    }
-    #endregion
+		return View("ShowFeed", userNewPreferences);
+	}
+	#endregion
 
-    #region Private Methods
-    private UserPreferences generateUserPreferences(string currentFilter, int pageNumber, string searchTerm)
-    {
-        ResultFilter deserializedFilter = deserializeFilter(currentFilter);
-        PaginatedList<IFeedItem> currentPageFeedItems = getFilteredList(deserializedFilter, pageNumber, searchTerm);
-        UserPreferences userNewPreferences = getUserPreferences(currentPageFeedItems, deserializedFilter, searchTerm);
+	#region Private Methods
+	private UserPreferences getDefaultUserPreferences()
+	{
+		string lastFilter = getFilterFromCookie();
 
-        return userNewPreferences;
-    }
+		return generateUserPreferences(
+			 currentFilter: lastFilter,
+			 searchTerm: string.Empty,
+			 pageNumber: 1);
+	}
 
-    private PaginatedList<IFeedItem> getFilteredList(ResultFilter filter, int pageNumber, string searchTerm = "")
-    {
-        FeedPageSettings pageSettings = new FeedPageSettings()
-        {
-            SearchTerm = searchTerm.Trim(),
-            PageSize = this.pageSize,
-            PageNumber = pageNumber,
-            Filter = filter,
-        };
+	private UserPreferences generateUserPreferences(string currentFilter, int pageNumber, string searchTerm)
+		=>
+		new UserPreferencesGenerator()
+		.SetPageSize(this.pageSize)
+		.GenerateUserPreferences(currentFilter, pageNumber, searchTerm);
 
-        return getFeedItems(pageSettings);
-    }
+	private UserPreferences convertFilterToUserPrefs(ResultFilter filter)
+		=>
+		new UserPreferencesGenerator()
+		.SetPageSize(this.pageSize)
+		.ConvertFilterToUserPrefs(filter);
 
-    private ResultFilter deserializeFilter(string filter)
-    {
-        Func<ResultFilter> deserialization = () =>
-        {
-            try
-            {
-                return JsonConvert.DeserializeObject<ResultFilter>(filter)
-                    ?? new ResultFilter();
-            }
-            catch (Exception ex)
-            {
-                return new ResultFilter();
-            }
-        };
+	private void handleFilterMemory(UserPreferences userPreferences, string btnValue)
+		=>
+		new CookieManager()
+		.SetFilterCookieName(this.filterCookieName)
+		.HandleFilterMemory(userPreferences, btnValue, base.Response);
 
-        return string.IsNullOrEmpty(filter)
-            ? new ResultFilter()
-            : deserialization.Invoke();
-    }
-
-    private void saveFilterInCookie(ResultFilter filter)
-    {
-        string deserializedFilter = filter.ToString();
-
-        Response
-            .Cookies
-            .Append(
-                key: this.filterCookieName,
-                value: deserializedFilter,
-                options: new CookieOptions()
-                {
-                    Expires = DateTime.Now.AddMonths(12)
-                });
-    }
-
-    private PaginatedList<IFeedItem> getFeedItems(FeedPageSettings pageSettings)
-        =>
-        new FeedProvider()
-        .GetFeedItems(pageSettings);
-
-    private string getFilterFromCookie()
-        =>
-        Request.Cookies[this.filterCookieName] ?? string.Empty;
-
-    private UserPreferences getUserPreferences(PaginatedList<IFeedItem> feedItems, ResultFilter filter, string searchTerm = "")
-        =>
-        new UserPreferences()
-        {
-            PaginatedList = feedItems,
-            SearchTerm = searchTerm,
-            Filter = filter
-        };
-    #endregion
+	private string getFilterFromCookie()
+		=>
+		new CookieManager()
+		.SetFilterCookieName(this.filterCookieName)
+		.GetFilterFromCookie(base.Request);
+	#endregion
 }
